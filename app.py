3eb8e13b7a567ae40eafd1ddcaa7004f7a1830b0
Princy Jaat <princy8524145@jmieti.edu.in>
2026-06-29 13:57:11 +0530

    Change Flask app to run on all interfaces

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 if (__name__ == "__main__"):
-    # app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+    # app.run(debug=True)
 
     # print("hello")
